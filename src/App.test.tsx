cf890759d8c66cf543bd870eb45e5dc995ac5de6
nknrw <nknrw@yandex.ru>
2023-09-05 18:50:18 +0300

    fix: tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Add task', () => {
+  render(<App/>);
+  const input = screen.getByPlaceholderText('New Task');
+  const button = screen.getByText('Add');
+  fireEvent.change(input, {target: {value: 'Some task'}});
+  fireEvent.click(button);
+  expect(screen.getByText('Some task')).toBeInTheDocument();
+  expect(localStorage.getItem('tasks')).toContain('"text":"Some task"');
+});
+
+test('Toggle task status', () => {
+  render(<App/>);
+  const checkbox = screen.getByTestId('checkbox');
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(localStorage.getItem('tasks')).toContain('"finished":true');
+});
+
+test('Remove task', () => {
+  render(<App/>);
+  const deleteButton = screen.getByText('Remove');
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText('Some task')).not.toBeInTheDocument();
+  expect(localStorage.getItem('tasks')).not.toContain('Some task');
+});
+
+test('Toggle tabs', () => {
+  render(<App/>);
+  const allTab = screen.getByText('All');
+  const unfinishedTab = screen.getByText('Unfinished');
+  const finishedTab = screen.getByText('Finished');
+  fireEvent.click(allTab);
+  expect(allTab).toHaveClass('active');
+  fireEvent.click(unfinishedTab);
+  expect(unfinishedTab).toHaveClass('active');
+  fireEvent.click(finishedTab);
+  expect(finishedTab).toHaveClass('active');
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './components/Header';
+import Tabs from './components/Tabs'; // Импортируйте компонент Tabs
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
+import {Task} from './types/types';
 
-function App() {
+const App: React.FC = () => {
+  const savedTasks = localStorage.getItem('tasks');
+  const initialTasks: Task[] = savedTasks ? JSON.parse(savedTasks) : [];
+
+  const [tasks, setTasks] = useState<Task[]>(initialTasks);
+  const [newTask, setNewTask] = useState('');
+  const [activeTab, setActiveTab] = useState('All');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      const newTaskObj: Task = {
+        id: Date.now(),
+        text: newTask,
+        finished: false,
+      };
+
+      // Вставляем новую задачу в начало текущего списка задач
+      const updatedTasks = [newTaskObj, ...tasks];
+      setTasks(updatedTasks);
+      setNewTask('');
+
+      // Сохраняем обновленный список задач в локальное хранилище
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    }
+  };
+
+  const toggleTask = (id: number) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? {...task, finished: !task.finished} : task
+    );
+    setTasks(updatedTasks);
+
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      addTask();
+    }
+  };
+
+  const deleteTask = (id: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
+  const unfinishedTasks = tasks.filter((task) => !task.finished);
+  const finishedTasks = tasks.filter((task) => task.finished);
+
+  let displayedTasks: Task[] = tasks;
+  if (activeTab === 'Unfinished') {
+    displayedTasks = unfinishedTasks;
+  } else if (activeTab === 'Finished') {
+    displayedTasks = finishedTasks;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header/>
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab}/>
+      <TaskInput
+        newTask={newTask}
+        handleNewTaskChange={(text) => setNewTask(text)}
+        addTask={addTask}
+        handleKeyPress={handleKeyPress}
+      />
+
+      <TaskList
+        tasks={displayedTasks}
+        toggleTask={toggleTask}
+        deleteTask={deleteTask}
+      />
     </div>
   );
-}
+};
 
 export default App;
